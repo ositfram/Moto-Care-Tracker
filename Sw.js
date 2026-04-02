e61b03511a645f43c9eceab3387915107df3d259
ositfram <adil_raihan@yahoo.com>
2026-04-02 22:44:08 +0800

    Update Sw.js

--- Sw.js
+++ Sw.js
@@ -1,12 +1,10 @@
-const CACHE_NAME = 'moto-care-v1';
+const CACHE_NAME = 'motocare-v1';
 const assets = ['index.html', 'manifest.json'];
 
-// ফাইলগুলো ক্যাশ করা যাতে অফলাইনেও চলে
-self.addEventListener('install', e => {
+self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-// ব্যাকগ্রাউন্ডে জিপিএস বা ডাটা প্রসেস করার অনুমতি
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
