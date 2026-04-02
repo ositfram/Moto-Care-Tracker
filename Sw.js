16c26cc65c27263b56b91bf36f583d92d2b976b4
ositfram <adil_raihan@yahoo.com>
2026-04-02 22:54:25 +0800

    Update Sw.js

--- Sw.js
+++ Sw.js
@@ -1,10 +1,18 @@
-const CACHE_NAME = 'motocare-v1';
-const assets = ['index.html', 'manifest.json'];
+const CACHE_NAME = 'motocare-v2';
+const assets = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
